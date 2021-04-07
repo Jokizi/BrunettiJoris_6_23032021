fe57957ecb68cc0b180ba93be929593aef4cf139
BRUNETTI <joris.brunetti@gmail.com>
2021-04-07 11:52:05 +0200

    lien backend-frontend

---
+++ back-end/models/thing.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const thingSchema = mongoose.Schema({
+  //sauce: { type: Object, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  heat: { type: Number, required: true },
+  imageUrl: { type: String, required: true },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  mainPepper: { type: String, required: true },
+  usersLiked: { type: [String] },
+  usersDisliked: { type: [String] },
+  userId: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Thing", thingSchema);
