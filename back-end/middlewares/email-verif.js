4144b1107e007d29d0b126bda6f2e4fbbeb19438
Jokizi <72267511+Jokizi@users.noreply.github.com>
2021-10-04 15:22:30 +0200

    add images gitignore

--- back-end/middlewares/email-verif.js
+++ back-end/middlewares/email-verif.js
@@ -3,8 +3,6 @@ const emailValidator = require("email-validator");
 // validation champs email via 'npm email-validator'
 
 module.exports = (req, res, next) => {
-  console.log(!emailValidator.validate(req.body.email));
-  console.log("------------------------------------");
   if (!emailValidator.validate(req.body.email)) {
     res.writeHead(400, '{"message":"Mauvaise saisie !"}');
     res.end('{"message":"Renseigner une adresse mail valide"}');
