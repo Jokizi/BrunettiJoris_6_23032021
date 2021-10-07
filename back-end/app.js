aec3f56a52b164ada4001599342a5a593d0c28bd
Jokizi <72267511+Jokizi@users.noreply.github.com>
2021-10-07 15:13:31 +0200

    add express-rate-limit

--- back-end/app.js
+++ back-end/app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const Thing = require("./models/thing");
 const saucesRoutes = require("./routes/sauces");
 const userRoutes = require("./routes/user");
+
 require("dotenv").config();
 
 mongoose
@@ -23,10 +24,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   next();
 });
 
--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const emailVerif = require("../middlewares/email-verif");
 const passwordVerif = require("../middlewares/password-verif");
 const userCtrl = require("../controllers/user");
-const ExpressBrute = require("express-brute");
-const store = new ExpressBrute.MemoryStore();
-const bruteforce = new ExpressBrute(store);
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 4, // limit each IP to 100 requests per windowMs
+});
 
 router.post("/signup", emailVerif, passwordVerif, userCtrl.signup);
-router.post("/login", bruteforce.prevent, userCtrl.login);
+router.post("/login", limiter, userCtrl.login);
 
 module.exports = router;
